36df5c5ae84397cc228b903804708d44f17a3b86
Matteo Biondi <matteobiondi1197@gmail.com>
2023-10-24 16:29:05 +0200

    Full adder design commented

--- Design/VHDL/src/subcomponents/full_adder.vhd
+++ Design/VHDL/src/subcomponents/full_adder.vhd
@@ -1,19 +1,35 @@
+--------------------------------------------------------------
+-- Packages list
+--------------------------------------------------------------
 library IEEE;
   use IEEE.std_logic_1164.all;
 
+--------------------------------------------------------------
+-- Component Description
+-- This VHDL file describe a full adder that take two inputs of
+-- 1 bit each, a cin bit and gives a sum bit and a carry out 
+-- bit as output
+--------------------------------------------------------------
 entity full_adder is 
   port(
+    -- INPUT --
     fa_a     : in std_logic;
     fa_b     : in std_logic;
     fa_cin   : in std_logic;
+    -- OUTPUT --
     fa_cout  : out std_logic;
     fa_s     : out std_logic
   );
 end entity;
 
-architecture fa_arch for full_adder is
+--------------------------------------------------------------
+-- Architecture declaration
+--------------------------------------------------------------
+architecture fa_arch of full_adder is
 begin
+    -- Sum result
     fa_s    <= fa_a xor fa_b xor fa_cin;
+    -- Carry out result
     fa_cout <= (fa_a and fa_b) or (fa_a and fa_cin) or (fa_b and fa_cin);
 
 end architecture;
