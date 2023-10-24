cce4af34744d62520e7afec546afdd925b9d10f1
Matteo Biondi <matteobiondi1197@gmail.com>
2023-10-24 16:31:28 +0200

    DFF design commented

--- Design/VHDL/src/macrocomponents_simple/d_flip_flop_n.vhd
+++ Design/VHDL/src/macrocomponents_simple/d_flip_flop_n.vhd
@@ -1,17 +1,15 @@
 --------------------------------------------------------------
 -- Packages list
 --------------------------------------------------------------
-
 library IEEE;
   use IEEE.std_logic_1164.all; 
 
 --------------------------------------------------------------
 -- Component Description
--- This VHDL file describe a D-edge triggered FF with N bits.
+-- This VHDL file describes a D-edge triggered FF with N bits.
 -- The component has asynchronous reset and an 'enable' port 
 -- as input. 
 --------------------------------------------------------------
-
 entity d_flip_flop_n is
   generic (
     -- Number of register bits
@@ -21,26 +19,36 @@ entity d_flip_flop_n is
     DFF_N_BITS : natural := 11
   );
   port (
-    dff_clk         : in std_logic;
-    dff_async_rst_n : in std_logic;
-    dff_en          : in std_logic;
-    dff_d           : in std_logic_vector(DFF_N_BITS - 1 downto 0);
-    
-    dff_q           : out std_logic_vector(DFF_N_BITS - 1 downto 0)
+    -- INPUT --
+    dff_clk         : in std_logic;                                   -- Clock
+    dff_async_rst_n : in std_logic;                                   -- Asynchronous reset low
+    dff_en          : in std_logic;                                   -- Enable
+    dff_d           : in std_logic_vector(DFF_N_BITS - 1 downto 0);   -- Input data
+    -- OUTPUT --
+    dff_q           : out std_logic_vector(DFF_N_BITS - 1 downto 0)   -- Output data
   );
 end entity;
 
+
+--------------------------------------------------------------
+-- Architecture declaration
+--------------------------------------------------------------
 architecture dff_arch of d_flip_flop_n is
 begin
 
-  p_DFF: process(dff_clk, dff_async_rst_n) begin
+  p_DFF: process(dff_clk, dff_async_rst_n) 
+  begin
+    
     if (dff_async_rst_n = '0') then
+      -- Reset case
       dff_q <= (others => '0');
     elsif (rising_edge(dff_clk)) then
+      -- Clock rising edge occurs
       if (dff_en = '1') then
         dff_q <= dff_d;
       end if;
     end if;
+  
   end process;
 
 end architecture;
