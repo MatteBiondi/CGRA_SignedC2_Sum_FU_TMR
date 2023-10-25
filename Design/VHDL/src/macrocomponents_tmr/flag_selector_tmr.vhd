a15a26200c375aa57ee8351fffe156658eba8e74
Matteo Biondi <matteobiondi1197@gmail.com>
2023-10-25 11:43:11 +0200

    Design for flag selector with TMR

--- Design/VHDL/src/macrocomponents_tmr/flag_selector_tmr.vhd
+++ Design/VHDL/src/macrocomponents_tmr/flag_selector_tmr.vhd
@@ -1,13 +1,17 @@
 --------------------------------------------------------------
 -- Packages list
 --------------------------------------------------------------
-
 library IEEE;
   use IEEE.std_logic_1164.all; 
 
 --------------------------------------------------------------
 -- Component Description
--- 
+-- This VHDL file describe a tmr version for a multiplexer to 
+-- decide which N-bit flag to output as the sum operation. 
+-- The choice is based on a configuration word passed in input. 
+-- The component has three input flags (two from the operands 
+-- and one from the operation result), an input configuration 
+-- word, and an output flag.
 --------------------------------------------------------------
 
 entity flag_selector_tmr is
@@ -18,17 +22,22 @@ entity flag_selector_tmr is
     FLG_SLC_TMR_CFG_WD_N_BITS : positive  :=  2;
     -- The description only support N=3 modular redundancy
     -- TMR by default
-    FLG_SLC_TMR_N_MODULES     : positive := 3 
+    FLG_SLC_TMR_N_MODULES     : positive  := 3 
   );
   port(
-    flg_slc_tmr_from_rca  : in std_logic_vector(FLG_SLC_TMR_FLAG_N_BITS-1 downto 0);
-    flg_slc_tmr_operand_1 : in std_logic_vector(FLG_SLC_TMR_FLAG_N_BITS-1 downto 0);
-    flg_slc_tmr_operand_2 : in std_logic_vector(FLG_SLC_TMR_FLAG_N_BITS-1 downto 0);
-    flg_slc_tmr_conf_wd   : in std_logic_vector(FLG_SLC_TMR_CFG_WD_N_BITS-1 downto 0);
-    flg_slc_tmr_res       : out std_logic_vector(FLG_SLC_TMR_FLAG_N_BITS-1 downto 0)
+    -- INPUT --
+    flg_slc_tmr_from_rca  : in std_logic_vector(FLG_SLC_TMR_FLAG_N_BITS-1 downto 0);    -- Flag from rca
+    flg_slc_tmr_operand_1 : in std_logic_vector(FLG_SLC_TMR_FLAG_N_BITS-1 downto 0);    -- Flag operand 1
+    flg_slc_tmr_operand_2 : in std_logic_vector(FLG_SLC_TMR_FLAG_N_BITS-1 downto 0);    -- Flag operand 2
+    flg_slc_tmr_conf_wd   : in std_logic_vector(FLG_SLC_TMR_CFG_WD_N_BITS-1 downto 0);  -- Configuration word
+    -- OUTPUT --
+    flg_slc_tmr_res       : out std_logic_vector(FLG_SLC_TMR_FLAG_N_BITS-1 downto 0)    -- Flag result
   );
 end entity;
 
+--------------------------------------------------------------
+-- Architecture declaration
+--------------------------------------------------------------
 architecture flag_slc_tmr_arch of flag_selector_tmr is
   --------------------------------------------------------------
   -- Types definition
@@ -36,12 +45,13 @@ architecture flag_slc_tmr_arch of flag_selector_tmr is
   -- Each std_logic_vector has as many elements as the number of bit of each flag_selector
   -- There are as many array elements as the number of redundant modules
   type FLG_SLC_TMR_ARRAY_TYPE is array (0 to FLG_SLC_TMR_N_MODULES-1) 
-    of std_logic_vector(FLG_SLC_TMR_RES_N_BITS-1 downto 0);
+    of std_logic_vector(FLG_SLC_TMR_FLAG_N_BITS-1 downto 0);
   
   
   --------------------------------------------------------------
   -- Signals declaration
   --------------------------------------------------------------
+  -- Signal for outputs from flag selectors
   signal internal_flag_res  : FLG_SLC_TMR_ARRAY_TYPE;
 
 
@@ -84,12 +94,11 @@ begin
   -- Majority vote election for 3 input data of FLG_SLC_TMR_N_BITS each
   -- Formula:
   -- Input: x,y,z | Output: r | result: r = NAND(NAND(x,y), NAND(x,z), NAND(y,z)) 
-
-  flg_slc_tmr_res   <=  NAND(
-                          NAND(internal_flag_res(0),internal_flag_res(1)),
-                          NAND(internal_flag_res(1),internal_flag_res(2)),
-                          NAND(internal_flag_res(0),internal_flag_res(2))
-                        );
+  -- Equivalent to r = xy + yz + xz
+  
+  flg_slc_tmr_res   <=  (internal_flag_res(0) and internal_flag_res(1)) or 
+                        (internal_flag_res(1) and internal_flag_res(2)) or
+                        (internal_flag_res(0) and internal_flag_res(2));
   
   
 end architecture;
